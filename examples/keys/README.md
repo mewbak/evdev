43a6bc691857d9be0e06424ac45502c86dc7aa97
Jim Teeuwen <jimteeuwen@gmail.com>
2013-09-07 14:52:05 +0200

    Adds examples/keys program. It demonstrates how to read and interpret the global key/button state from a device that supports it.

---
+++ examples/keys/README.md
@@ -0,0 +1,11 @@
+## Keys
+
+This program demonstrates how to read the global key- and button state
+from an input device.
+
+
+### Usage
+
+	$ go build
+	$ ./keys /dev/input/event0
+
---
+++ examples/keys/main.go
@@ -0,0 +1,89 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/jteeuwen/evdev"
+	"os"
+	"time"
+)
+
+func main() {
+	node := parseArgs()
+
+	// Create and open our device.
+	dev, err := evdev.Open(node)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		return
+	}
+
+	// Make sure it is closed once we are done.
+	defer dev.Close()
+
+	// Ensure this device supports key/button events.
+	if !hasKeys(dev) {
+		fmt.Fprintf(os.Stderr, "Device %q does not support key/button events.\n", node)
+		return
+	}
+
+	// Fetch the current key/button state and display it.
+	ks := dev.KeyState()
+	listState(ks)
+
+	<-time.After(1e9)
+}
+
+// hasKeys determines if the given device supports key/button state events.
+func hasKeys(dev *evdev.Device) bool {
+	events := dev.EventTypes()
+
+	for n := uint(0); n < evdev.EvMax; n++ {
+		if events.Test(n) && n == evdev.EvKey {
+			return true
+		}
+	}
+
+	return false
+}
+
+// listState prints the global key/button state, as defined
+// in the given bitset.
+func listState(set evdev.Bitset) {
+	for n := uint(0); n < evdev.KeyMax; n++ {
+		// The key is considered pressed if the bitset
+		// has its corresponding bit set.
+		if !set.Test(n) {
+			continue
+		}
+
+		fmt.Printf("  Key 0x%02x ", n)
+
+		switch n {
+		case evdev.KeyReserved:
+			fmt.Printf("Reserved")
+		case evdev.KeyEscape:
+			fmt.Printf("Escape")
+		case evdev.BtnStylus2:
+			fmt.Printf("2nd stylus button")
+
+			// more keys/buttons..
+		}
+
+		fmt.Println()
+	}
+}
+
+func parseArgs() string {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <node>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	return flag.Args()[0]
+}
